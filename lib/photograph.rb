fec63078c85c9b8810d7694e9ac67e10ce1df996
rspringer47 <rspringer47@gmail.com>
2017-07-24 09:28:25 -0600

    iteration 1 done

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,2 +1,17 @@
+require 'pry'
+
 class Photograph
+
+  attr_reader :id,
+              :artist_id,
+              :museum_id,
+              :year
+
+  def initialize(photo_hash)
+    @id = photo_hash[:id]
+    @artist_id = photo_hash[:artist_id]
+    @museum_id = photo_hash[:museum_id]
+    @year = photo_hash[:year]
+  end
+
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,4 +4,28 @@ require 'minitest/pride'
 require './lib/photograph'
 
 class PhotographTest < Minitest::Test
+
+  def setup
+    @photograph = Photograph.new({id: 1,
+                                  name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                                  artist_id: 4,
+                                  museum_id: 2,
+                                  year: 1954})
+  end
+
+  def test_is_has_an_id
+    assert_equal 1, @photograph.id
+  end
+
+  def test_it_has_an_artist_id
+    assert_equal 4, @photograph.artist_id
+  end
+
+  def test_it_has_a_museum_id
+    assert_equal 2, @photograph.museum_id
+  end
+
+  def test_it_has_a_year
+    assert_equal 1954, @photograph.year
+  end
 end
